68811262eb53efb7c1afbe5f7e99d4b16ed4dd03
Evgeny Rudakov <eugenartemovich@gmail.com>
2018-12-19 01:09:36 +0300

    Done graphs and sum scripts

---
+++ lazy_and_eager_graphs.js
@@ -0,0 +1,60 @@
+// Может немного нелепо смотрится, что EagerGraph наследует LazyGraph, но он реализуется через его фунции,
+// поэтому решил оставить такое наследование и не создавать абстрактный класс.
+
+// Значение вершин кэшируется, ловит ошибки.
+
+
+const ARGS_REGEX = /([\w_-]+)/gm;
+
+function getParams(func) {
+    let funcString = func.toString();
+    let result = funcString.slice(funcString.indexOf('(') + 1, funcString.indexOf(')')).match(ARGS_REGEX);
+    if (result === null)
+        result = [];
+    return result;
+}
+
+class LazyGraph {
+
+    receiveGraph(graph) {
+        this.graph = graph;
+        this.parents = {};
+        this.values = {};
+        for (let vertex in this.graph)
+            this.parents[vertex] = getParams(this.graph[vertex]);
+        return this;
+    };
+
+    calcVertex(vertex) {
+        try {
+            if (!this.values[vertex]) {
+                let params = [];
+                for (let item of this.parents[vertex]) {
+                    params.push(this.calcVertex(item));
+                }
+                this.values[vertex] = this.graph[vertex](...params);
+
+            }
+            return this.values[vertex]
+        }
+        catch (e) {
+            if (e instanceof RangeError) {
+                throw new RangeError("Обнаружена кольцевая зависимость, идущая от " + vertex)
+            } else if (!this.graph) {
+                throw new RangeError("Граф пуст или не существует")
+            }else if (this.graph[vertex] === undefined) {
+                throw new RangeError("Нет такой вершины: " + vertex)
+            }
+
+        }
+    }
+}
+
+class EagerGraph extends LazyGraph {
+    receiveGraph(graph) {
+        super.receiveGraph(graph);
+        for (let vertex in this.graph)
+            this.calcVertex(vertex);
+        return this;
+    };
+}
---
+++ sum.js
@@ -0,0 +1,11 @@
+// Реализация рекурсивной фунции суммы без обращения к ней по имени и без побочной функции через callee
+
+function sum(a) {
+    if (!arguments.callee.store) {
+        arguments.callee.store = 0}
+    arguments.callee.store += a;
+    let func = arguments.callee;
+    arguments.callee.toString = function() {let res = func.store; func.store = 0; return res};
+    arguments.callee.valueOf = function() {let res = func.store; func.store = 0; return res};
+    return arguments.callee;
+}
